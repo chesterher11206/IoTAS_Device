3d777f3a50488190023cc0a9bc22780fb6e63bcf
chester11206 <b04705039@ntu.edu.tw>
2019-06-19 22:48:32 +0800

    device agv

--- agv_test.py
+++ agv_test.py
@@ -1,5 +1,9 @@
 import RPi.GPIO as GPIO
 import time
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import matplotlib.pyplot as plt
 
 # for front
 CONTROL_PIN = 17
