6e67883843f5a6db66bcb9fc128994f18260296c
chester11206 <b04705039@ntu.edu.tw>
2019-06-20 11:40:44 +0800

    agv picamera

--- agv_picamera.py
+++ agv_picamera.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import matplotlib.pyplot as plt
@@ -17,8 +18,12 @@ with PiCamera() as camera:
             print('Captured {}x{} image'.format(output.array.shape[1], output.array.shape[0]))
 
             crop_img = output.array
-            gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
-            plt.imshow(gray, cmap='gray')
+            hsv_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
+            low_range = np.array([0, 123, 100])
+            high_range = np.array([5, 255, 255])
+            range_img = cv2.inRange(hsv_img, low_range, high_range)
+            gray = cv2.cvtColor(range_img, cv2.COLOR_BGR2GRAY)
+            plt.image.imsave('~/name.png', gray)
             blur = cv2.GaussianBlur(gray, (5, 5), 0)
             ret, thresh = cv2.threshold(blur, 60, 255, cv2.THRESH_BINARY_INV)
 
@@ -46,3 +51,4 @@ with PiCamera() as camera:
             # cv2.waitKey(1)
 
             output.truncate(0)
+            break
